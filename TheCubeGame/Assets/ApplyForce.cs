d4d4742cd8a2169c7e4dedf2964577d306ef4668
STUDENT Brian Purcell <Brian.Purcell@students.ittralee.ie>
2022-10-05 12:44:01 +0100

    Signed-off-by: STUDENT Brian Purcell <Brian.Purcell@students.ittralee.ie>

---
+++ TheCubeGame/Assets/ApplyForce.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplyForce : MonoBehaviour
+{
+    Rigidbody ourRigidBody;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ourRigidBody = GetComponent<Rigidbody>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            ourRigidBody.AddExplosionForce(100, transform.position + Vector3.down, 2);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        collision.transform.position += Vector3.down;
+    }
+}
