fdf5afc20c0565649ac39620261578208ca82089
STUDENT Brian Purcell <Brian.Purcell@students.ittralee.ie>
2022-10-06 10:50:56 +0100

    Signed-off-by: STUDENT Brian Purcell <Brian.Purcell@students.ittralee.ie>

--- TheCubeGame/Assets/ApplyForce.cs
+++ TheCubeGame/Assets/ApplyForce.cs
@@ -27,6 +27,20 @@ public class ApplyForce : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        collision.transform.position += Vector3.down;
+      Health objectHitHealth =  collision.gameObject.GetComponent<Health>();
+
+        if (objectHitHealth)
+        {
+            print("Found Health script in object hit");
+            objectHitHealth.takeDamage(20);
+
+            int ObjectsMaxHealth = objectHitHealth.whatsYourMaxHealth();
+            if (ObjectsMaxHealth > 100)
+                objectHitHealth.takeDamage(100);
+        }
+        else
+        {
+            print("Didn't find Health script in object hit");
+        }
     }
 }
